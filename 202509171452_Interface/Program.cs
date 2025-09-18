89e90ca70bd2b50780d76eb65526d3a635e15c8b
alakdjs <9765cass@naver.com>
2025-09-18 14:27:09 +0900

    2025-09-17

---
+++ 202509171452_Interface/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _202509171452_Interface
+{
+    // interface
+    // 행동규약
+    // 1. 인터페이스는 메소드, 이벤트, 인덱서, 프로퍼티만 가질 수 있습니다.
+    // 2. 접근제어자를 사용할 수 없고, 모든 겂이 public 으로 선언됩니다.
+    // 3. 구현부가 없습니다.
+    // 4. interface를 추가한 클래스는 interface에 선언된 메소드를 구현할 책임을 갖습니다.
+    // 5. interface는 다중으로 추가할 수 있습니다.
+
+    interface Move
+    {
+        void Start();
+        void Stop();
+        void Acceleration();
+    }
+
+    class Car : Move
+    {
+        private string _modelName;
+        private float _maxSpeed;
+
+        public Car(string modelName, float maxSpeed)
+        {
+            _modelName = modelName;
+            _maxSpeed = maxSpeed;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine($"{_modelName}이 출발합니다.");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine($"{_modelName}이 정지합니다.");
+        }
+
+        public void Acceleration()
+        {
+            Console.WriteLine($"{_modelName}이 가속을 합니다.");
+        }
+    }
+
+    class Ship : Move
+    {
+        private string _name;
+        private float _maxSpeed;
+
+        public Ship(string name, float maxSpeed)
+        {
+            _name = name;
+            _maxSpeed = maxSpeed;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine($"{_name}이 출항합니다.");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine($"{_name}이 입항합니다.");
+        }
+
+        public void Acceleration()
+        {
+            Console.WriteLine($"{_name}이 가속합니다.");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("GV80", 280.0f);
+            Ship ship = new Ship("쾌속선", 50.0f);
+
+            Move move = car;
+
+            move.Start();
+            move.Stop();
+            move.Acceleration();
+
+            move = ship;
+
+            move.Start();
+            move.Stop();
+            move.Acceleration();
+
+            Console.WriteLine();
+
+            Move[] moveArray = new Move[] {car, ship};
+
+            foreach(var obj in moveArray)
+            {
+                obj.Start();
+                obj.Acceleration();
+                obj.Stop();
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 202509171508_MultiInterface/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _202509171508_MultiInterface
+{
+    // 멀티 인터페이스
+
+    public abstract class Run // 추상 클래스
+    {
+        public abstract void Walk();  // 추상 메소드
+    }
+
+    public class Human : Run
+    {
+        public override void Walk()
+        {
+            Console.WriteLine($"사람이 걷는다.");
+        }
+    }
+
+    class A
+    {
+        private int _value;
+
+        public int Value { get { return _value; } set { _value = value; } }
+    }
+
+    interface IRunnable
+    {
+        int Speed { set; get; } // 자동구현 프로퍼티
+        void Run();
+    }
+
+    interface IFlyable
+    {
+        void Fly();
+    }
+
+    class FlyingCar : IRunnable, IFlyable
+    {
+        public int Speed { set; get; }
+
+        public void Run()
+        {
+            Console.WriteLine("Run! Run!");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("Fly! Fly!");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FlyingCar car = new FlyingCar();
+
+            car.Run();
+            car.Fly();
+
+            IRunnable runnable = car;
+            runnable.Run();
+
+            IFlyable flyable = car;
+            flyable.Fly();
+
+            Human human = new Human();
+            human.Walk();
+        }
+    }
+}
